ff5a9ef7adf598a3b74f36978d75bafd11bfe108
Tomasz Kołakowski <01171631@pw.edu.pl>
2025-11-05 11:39:09 +0100

    Rozwiązanie

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,9 +4,18 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-
-
-
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar==0)
+        return 0;
+    Stocznia stocznia{};
+    unsigned int l_trans = 0;    
+    unsigned int l_zaglowcow = 0;
+    while (l_trans < towar) {
+        Statek* statek = stocznia();
+        if (Zaglowiec* zaglowiec = dynamic_cast< Zaglowiec* >(statek)) {
+            l_zaglowcow++;
+        }
+        l_trans += statek->transportuj();
+        delete statek;
+    }
+    return l_zaglowcow;
 }
